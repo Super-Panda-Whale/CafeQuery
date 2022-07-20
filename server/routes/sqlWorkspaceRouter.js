efbe78ba777338a1d559d1d718015b0eece354d2
Thomas Ho <tdho.uh@gmail.com>
2022-07-20 10:41:45 -0500

    changes commited

--- server/routes/sqlWorkspaceRouter.js
+++ server/routes/sqlWorkspaceRouter.js
@@ -3,11 +3,7 @@ const sqlWorkSpaceController = require('../controllers/sqlWorkspaceController');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-<<<<<<< HEAD
   return res.json('hello joey');
-=======
-  return res.status(200).json('hello world')
->>>>>>> development
 });
 
 router.post('/', sqlWorkSpaceController.createWorkspace, (req, res) => {
