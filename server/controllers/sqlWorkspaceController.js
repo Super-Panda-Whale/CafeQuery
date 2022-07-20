1f2b53b6369bcfe7a1b5fe284cfc8cd14f9ba5d0
Thomas Ho <tdho.uh@gmail.com>
2022-07-20 10:35:31 -0500

    updates

--- server/controllers/sqlWorkspaceController.js
+++ server/controllers/sqlWorkspaceController.js
@@ -3,8 +3,21 @@ const sqlDB = require('../models/sqlModels.js');
 
 const sqlWorkspaceController = {};
 
+sqlWorkspaceController.getWorkspace = async function (req, res, next) {
+  console.log(' in get workspaces midware!!!!');
+  const { zipcode } = req.params;
+  const queryString = `SELECT * FROM workspaces WHERE zipcode = ${zipcode}`;
+  try {
+    const workspaces = await sqlDB.query(queryString);
+    res.locals.workspaces = workspaces.rows;
+    return next();
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
 sqlWorkspaceController.createWorkspace = async function (req, res, next) {
-  // console.log('UP HERE BITCH');
   const {
     workspaceName,
     zipcode,
@@ -27,16 +40,18 @@ sqlWorkspaceController.createWorkspace = async function (req, res, next) {
   } = req.body;
   const queryString = `
     INSERT INTO workspaces(WorkspaceName, Zipcode, Address, Rating, Wifi, Type, Quiet, Outlets, TimeLimit, LaptopRestrictions, Crowded, OutdoorSeating, PetFriendly, URL, FoodRating, CoffeeRating, Seating, Other)
-    VALUES('${workspaceName}', '${zipcode}', '${address}', '${rating}', '${wifi}', '${type}', '${quiet}', '${outlets}', '${timeLimit}', '${laptopRestrictions}', '${crowded}', '${outdoorSeating}', '${petFriendly}', '${url}', '${foodRating}', '${coffeeRating}', '${seating}', '${other}')`;
-  // console.log('Im here');
-  const testQuery = `INSERT INTO test(testName, octopusName) VALUES ('${req.body.testName}', '${req.body.octopusName}') RETURNING *`;
+    VALUES('${workspaceName}', '${zipcode}', '${address}', '${rating}', '${wifi}', '${type}', '${quiet}', '${outlets}', '${timeLimit}', '${laptopRestrictions}', '${crowded}', '${outdoorSeating}', '${petFriendly}', '${url}', '${foodRating}', '${coffeeRating}', '${seating}', '${other}') RETURNING *`;
   try {
     const result = await sqlDB.query(queryString);
     res.locals.newWorkspace = result.rows;
     return next();
   } catch (err) {
     console.log(err);
-    next(err);
+    next({
+      log: err + ' error in the createWorkspace Middleware',
+      status: 404,
+      message: { err: 'You have a stupid error: ', err },
+    });
   }
 };
 module.exports = sqlWorkspaceController;
